801f957932bf101dc83654f7601651b25a2b71de
AlexVinster <123768269+AlexVinster@users.noreply.github.com>
2023-09-24 19:49:05 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,25 +8,82 @@ const mainContent = document.querySelector('.wrapper');
 
 // Додайте обробники подій для відкриття та закриття меню
 burgerMenu.addEventListener('click', () => {
-    mobileMenu.style.right = '0px'; // Відкрити меню
-    mainContent.style.filter = 'blur(5px)'; // Додайте блюр до основного контенту
-    mainContent.style.pointerEvents = 'none'; // Заблокуйте взаємодію з основним контентом
-    document.body.style.overflow = 'hidden'; // Забороніть скролінг сторінки
+  mobileMenu.style.right = '0px'; // Відкрити меню
+  mainContent.style.filter = 'blur(5px)'; // Додайте блюр до основного контенту
+  mainContent.style.pointerEvents = 'none'; // Заблокуйте взаємодію з основним контентом
+  document.body.style.overflow = 'hidden'; // Забороніть скролінг сторінки
 });
 
 closeButton.addEventListener('click', () => {
-    mobileMenu.style.right = '-300px'; // Закрити меню
-    mainContent.style.filter = 'none'; // Зняти блюр з основного контенту
-    mainContent.style.pointerEvents = 'auto'; // Дозволити взаємодію з основним контентом
-    document.body.style.overflow = 'auto'; // Дозволити скролінг сторінки
+  mobileMenu.style.right = '-300px'; // Закрити меню
+  mainContent.style.filter = 'none'; // Зняти блюр з основного контенту
+  mainContent.style.pointerEvents = 'auto'; // Дозволити взаємодію з основним контентом
+  document.body.style.overflow = 'auto'; // Дозволити скролінг сторінки
 });
+
+// Анімація
+
+const up = document.querySelectorAll('.animation-up');
+const left = document.querySelectorAll('.animation-left');
+
+function checkUp() {
+  up.forEach((element) => {
+    const sectionTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (sectionTop < windowHeight / 2) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+  });
+}
+
+function checkLeft() {
+  left.forEach((element) => {
+    const sectionTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (sectionTop < windowHeight / 2) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkUp);
+window.addEventListener('scroll', checkLeft);
+
+checkUp();
+checkLeft();
 
 // Функція для визначення інформації про операційну систему
 function getOSInfo(userAgent) {
-    const platformInfo = platform.parse(userAgent);
-    const osName = platformInfo.os.family;
-    const osVersion = platformInfo.os.version;
-    return `${osName} ${osVersion}`;
+  const os = {
+    Windows: /Windows NT (\d+\.\d+)/i,
+    macOS: /Mac OS X (\d+[_\.\d]+)/i,
+    Linux: /Linux/i,
+    Android: /Android (\d+\.\d+)/i,
+    iOS: /OS (\d+[._\d+]*) like Mac OS X/i,
+  };
+
+  for (const platform in os) {
+    if (os[platform].test(userAgent)) {
+      let osVersion = '';
+      if (platform === 'Windows') {
+        osVersion = userAgent.match(/Windows NT (\d+\.\d+)/i)[1];
+      } else if (platform === 'macOS') {
+        osVersion = userAgent.match(/Mac OS X (\d+[_\.\d]+)/i)[1].replace(/_/g, '.');
+      } else if (platform === 'Android') {
+        osVersion = `Android ${userAgent.match(/Android (\d+\.\d+)/i)[1]}`;
+      } else if (platform === 'iOS') {
+        osVersion = `iOS ${userAgent.match(/OS (\d+[._\d+]*) like Mac OS X/i)[1]}`;
+      }
+      return `${platform} ${osVersion}`;
+    }
+  }
+  return 'Невідома ОС';
 }
 
 // Отримати інформацію про браузер та операційну систему користувача
@@ -36,22 +93,22 @@ const osInfo = getOSInfo(userAgent);
 
 // Функція для визначення інформації про браузер
 function getBrowserInfo(userAgent) {
-    const browsers = {
-        Chrome: /Chrome/i,
-        Firefox: /Firefox/i,
-        Edge: /Edg/i,
-        Safari: /Safari/i,
-        Opera: /Opera|OPR/i,
-        IE: /Trident|MSIE/i,
-    };
+  const browsers = {
+    Chrome: /Chrome/i,
+    Firefox: /Firefox/i,
+    Edge: /Edg/i,
+    Safari: /Safari/i,
+    Opera: /Opera|OPR/i,
+    IE: /Trident|MSIE/i,
+  };
 
-    for (const browser in browsers) {
-        if (browsers[browser].test(userAgent)) {
-            const version = userAgent.match(/(?:Chrome|Firefox|Edge|Safari|Opera|IE)\/([\d.]+)/i);
-            return `${browser} ${version ? version[1] : ''}`;
-        }
+  for (const browser in browsers) {
+    if (browsers[browser].test(userAgent)) {
+      const version = userAgent.match(/(?:Chrome|Firefox|Edge|Safari|Opera|IE)\/([\d.]+)/i);
+      return `${browser} ${version ? version[1] : ''}`;
     }
-    return 'Невідомий браузер';
+  }
+  return 'Невідомий браузер';
 }
 
 // Отримати елемент, де буде виведене повідомлення
@@ -71,27 +128,12 @@ overlay.style.display = 'flex';
 // Закриття вікна при натисканні на кнопку "Закрити"
 const close = document.getElementById('close');
 close.addEventListener('click', () => {
-    overlay.style.display = 'none';
+  overlay.style.display = 'none';
 });
 
 // Закриття вікна при кліку на зовнішній overlay
 overlay.addEventListener('click', (e) => {
-    if (e.target === overlay) {
-        overlay.style.display = 'none';
-    }
+  if (e.target === overlay) {
+    overlay.style.display = 'none';
+  }
 });
-
-//PLATFORM JS
-const platformInfo = platform.parse(navigator.userAgent);
-const osName = platformInfo.os.family; // Назва операційної системи (Android, iOS, тощо)
-const osVersion = platformInfo.os.version; // Версія операційної системи
-
-if (osName === 'Android') {
-    const androidModel = platformInfo.product; // Модель Android-пристрою
-    console.log(`Ваш Android-пристрій: ${androidModel}, версія ${osVersion}`);
-} else if (osName === 'iOS') {
-    const iosModel = platformInfo.product; // Модель iOS-пристрою
-    console.log(`Ваш iOS-пристрій: ${iosModel}, версія ${osVersion}`);
-} else {
-    console.log(`Ваша операційна система: ${osInfo}`);
-}
